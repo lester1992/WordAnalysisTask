bb86de0c9d0ad0ef1f4aa92ee76c0797bbb41afe
Lester Imperial <Lester@altitudeinnovations.com.au>
2019-12-05 13:45:12 +0800

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WordAnalysisTask
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblLongestWord = new System.Windows.Forms.Label();
+            this.lblShortestWord = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblLongestPalindrome = new System.Windows.Forms.Label();
+            this.lblShortestPalindrome = new System.Windows.Forms.Label();
+            this.lblTotalPalindromes = new System.Windows.Forms.Label();
+            this.ODFPath = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(392, 252);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "&Read File";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(32, 48);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(387, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(425, 48);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(42, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Longest Word:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Shortest Word:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Average (Word Length):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Longest Palindrome:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Shortest Palindrome:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Total Palidromes:";
+            // 
+            // lblLongestWord
+            // 
+            this.lblLongestWord.AutoSize = true;
+            this.lblLongestWord.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLongestWord.Location = new System.Drawing.Point(173, 81);
+            this.lblLongestWord.Name = "lblLongestWord";
+            this.lblLongestWord.Size = new System.Drawing.Size(0, 15);
+            this.lblLongestWord.TabIndex = 9;
+            // 
+            // lblShortestWord
+            // 
+            this.lblShortestWord.AutoSize = true;
+            this.lblShortestWord.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShortestWord.Location = new System.Drawing.Point(173, 106);
+            this.lblShortestWord.Name = "lblShortestWord";
+            this.lblShortestWord.Size = new System.Drawing.Size(0, 15);
+            this.lblShortestWord.TabIndex = 10;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(173, 130);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 15);
+            this.lblAverage.TabIndex = 11;
+            // 
+            // lblLongestPalindrome
+            // 
+            this.lblLongestPalindrome.AutoSize = true;
+            this.lblLongestPalindrome.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLongestPalindrome.Location = new System.Drawing.Point(173, 157);
+            this.lblLongestPalindrome.Name = "lblLongestPalindrome";
+            this.lblLongestPalindrome.Size = new System.Drawing.Size(0, 15);
+            this.lblLongestPalindrome.TabIndex = 12;
+            // 
+            // lblShortestPalindrome
+            // 
+            this.lblShortestPalindrome.AutoSize = true;
+            this.lblShortestPalindrome.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShortestPalindrome.Location = new System.Drawing.Point(173, 181);
+            this.lblShortestPalindrome.Name = "lblShortestPalindrome";
+            this.lblShortestPalindrome.Size = new System.Drawing.Size(0, 15);
+            this.lblShortestPalindrome.TabIndex = 13;
+            // 
+            // lblTotalPalindromes
+            // 
+            this.lblTotalPalindromes.AutoSize = true;
+            this.lblTotalPalindromes.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPalindromes.Location = new System.Drawing.Point(173, 208);
+            this.lblTotalPalindromes.Name = "lblTotalPalindromes";
+            this.lblTotalPalindromes.Size = new System.Drawing.Size(0, 15);
+            this.lblTotalPalindromes.TabIndex = 14;
+            // 
+            // ODFPath
+            // 
+            this.ODFPath.Filter = "txt files (*.txt)|*.txt";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 302);
+            this.Controls.Add(this.lblTotalPalindromes);
+            this.Controls.Add(this.lblShortestPalindrome);
+            this.Controls.Add(this.lblLongestPalindrome);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblShortestWord);
+            this.Controls.Add(this.lblLongestWord);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnRead);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dev Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblShortestWord;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblLongestPalindrome;
+        private System.Windows.Forms.Label lblShortestPalindrome;
+        private System.Windows.Forms.Label lblTotalPalindromes;
+        private System.Windows.Forms.OpenFileDialog ODFPath;
+        public System.Windows.Forms.Label lblLongestWord;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WordAnalysisTask
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            ODFPath.ShowDialog();
+            txtPath.Text = ODFPath.FileName;            
+        }
+       
+        private void btnRead_Click(object sender, EventArgs e)
+        {   
+            if (!string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                List<string> ArrWords = new List<string>();
+                List<string> Palidromes = new List<string>();               
+                using (StreamReader file = new StreamReader(txtPath.Text))
+                {
+                    int counter = 0;
+                    string ln;
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        ArrWords.Add(ln);
+                        if (isStringPalindrome(ln))
+                        {
+                            Palidromes.Add(ln);
+                        }
+                        counter++;
+                    }
+                    file.Close();                    
+                }               
+                lblLongestWord.Text = ArrWords.OrderByDescending(s => s.Length).First();
+                lblShortestWord.Text = ArrWords.OrderByDescending(s => s.Length).Last();
+                lblAverage.Text = ArrWords.Average(n => n.Length).ToString();
+                lblLongestPalindrome.Text = Palidromes.OrderByDescending(s => s.Length).First();
+                lblShortestPalindrome.Text = Palidromes.OrderByDescending(s => s.Length).Last();
+                lblTotalPalindromes.Text = Palidromes.Count().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please Select a file");
+            }            
+        }        
+        public bool isStringPalindrome(String input)
+        {
+            var reversed = new string(Enumerable.Range(1, input.Length).Select(i => input[input.Length - i]).ToArray());
+            return String.Compare(input, reversed, true) == 0;
+        }
+
+    }
+}
